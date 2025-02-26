9877a38a24a9e30a5e0d212f7a09936e8f8b00e2
VReach <fubukimegumi31@gmail.com>
2025-02-26 12:29:59 +0700

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
         Font1:[
           "Josefin Sans", "serif"
         ]
+      },
+      height:{
+        nav:[
+          "75px"
+        ]
+      },
+      backgroundColor:{
+        trans:[
+          "#1e9ac6"
+        ],
+        bga:[
+          "#1e77c6"
+        ]
+      },
+      textColor:{
+        t1:[
+          "#1e9ac6"
+        ]
       }
     },
   },
